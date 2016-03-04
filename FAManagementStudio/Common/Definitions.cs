a15f099ef00eaea925a48d5e45060efb0cb932fe
kowill <mob.koya@gmail.com>
2016-03-04 22:25:22 +0900

    change the UI

---
+++ FAManagementStudio/Common/Definitions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FAManagementStudio.Common
+{
+    public enum ConstraintsKeyKind { None, Primary, Foreign };
+}
