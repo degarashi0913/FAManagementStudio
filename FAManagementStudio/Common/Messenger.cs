bf4946e42fc9048e9e3ec30b957c9c60bb322996
kowill <mob.koya@gmail.com>
2016-05-24 00:34:14 +0900

    change logic

--- FAManagementStudio/Common/Messenger.cs
+++ FAManagementStudio/Common/Messenger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace FAManagementStudio.Common
 {
@@ -9,31 +10,53 @@ namespace FAManagementStudio.Common
     {
         private static readonly Messenger _instance = new Messenger();
         public static Messenger Instance { get { return _instance; } }
-        private Dictionary<Type, List<Delegate>> _actions = new Dictionary<Type, List<Delegate>>();
+        private Dictionary<Type, List<MessageAction>> _actions = new Dictionary<Type, List<MessageAction>>();
         public void Register<TMessage>(object recipient, Action<TMessage> action)
         {
             var type = typeof(TMessage);
             if (!_actions.ContainsKey(type))
             {
-                _actions.Add(type, new List<Delegate>());
+                _actions.Add(type, new List<MessageAction>());
             }
             var list = _actions[type];
 
-            list.Add(action);
+            list.Add(new MessageAction(recipient, action));
 
         }
         public void Send<TMessage>(TMessage message)
         {
             var actions = _actions[typeof(TMessage)].ToList();
-            foreach (Action<TMessage> item in actions)
+            foreach (var item in actions)
             {
-                item(message);
+                item.Execute(message);
             }
         }
 
         public void Unregister<TMessage>(object recipient)
         {
-            _actions[typeof(TMessage)].Clear();
+            var items = _actions[typeof(TMessage)].Where(x => x.Target == recipient);
+            foreach (var item in items)
+            {
+                _actions[typeof(TMessage)].Remove(item);
+            }
+
+        }
+    }
+
+    public class MessageAction
+    {
+        private object _target;
+        private Delegate _action;
+
+        public object Target { get { return _target; } }
+        public MessageAction(object target, Delegate action)
+        {
+            _target = target;
+            _action = action;
+        }
+        public void Execute<T>(T param)
+        {
+            _action.Method.Invoke(_target, new object[] { param });
         }
     }
 }
