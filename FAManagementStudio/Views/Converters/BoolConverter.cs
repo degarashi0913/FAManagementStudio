9e33cbb0c106783b03c9c3409ac4224ca47e4603
kowill <mob.koya@gmail.com>
2016-07-07 00:14:25 +0900

    Change ConnectionSettingsView

---
+++ FAManagementStudio/Views/Converters/BoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace FAManagementStudio.Views.Converters
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    public class BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return "";
+            return (string)value == "True";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
