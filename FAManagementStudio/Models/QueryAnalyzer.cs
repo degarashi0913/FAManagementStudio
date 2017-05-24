8993c259a5760e444c7d598e4d0b6f15fab4a5a5
kowill <mob.koya@gmail.com>
2017-05-25 00:42:03 +0900

    Change Query Analyze Logic

---
+++ FAManagementStudio/Models/QueryAnalyzer.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FAManagementStudio.Models
+{
+    public class QueryAnalyzer
+    {
+        public static string[] Analyze(string inputs)
+        {
+            var endIdx = 0;
+            var result = new List<string> { };
+            while (endIdx < inputs.Length)
+            {
+                var startIdx = endIdx;
+                var word = GetWord(ref inputs, ref startIdx, out endIdx);
+                switch (word)
+                {
+                    case var x when x == "\r\n":
+                        continue;
+                    //comment
+                    case var x when x.StartsWith("--"):
+                        new NewLineToken().StatementEnd(ref inputs, ref endIdx);
+                        break;
+                    case var x when x.StartsWith("create", StringComparison.OrdinalIgnoreCase) ||
+                                    x.StartsWith("recreate", StringComparison.OrdinalIgnoreCase) ||
+                                    x.StartsWith("alter", StringComparison.OrdinalIgnoreCase):
+                        new BlockToken().StatementEnd(ref inputs, ref endIdx);
+                        break;
+                    case var x when x.StartsWith("execute", StringComparison.OrdinalIgnoreCase):
+                        var secondIdx = endIdx;
+                        var next = GetWord(ref inputs, ref secondIdx, out endIdx);
+                        //ignore space
+                        if (next.StartsWith("block", StringComparison.OrdinalIgnoreCase))
+                        {
+                            new ExecuteBlockToken().StatementEnd(ref inputs, ref endIdx);
+                        }
+                        else
+                        {
+                            new NormalToken().StatementEnd(ref inputs, ref endIdx);
+                        }
+                        break;
+                    default:
+                        new NormalToken().StatementEnd(ref inputs, ref endIdx);
+                        break;
+                }
+                result.Add(inputs.Substring(startIdx, endIdx - startIdx));
+            }
+            return result.ToArray();
+        }
+
+        public static string GetWord(ref string statement, ref int startIdx, out int endIdx)
+        {
+            var startFlg = false;
+            endIdx = startIdx;
+            while (endIdx < statement.Length)
+            {
+                var ch = statement[endIdx];
+                switch (ch)
+                {
+                    //ignore space
+                    case ' ':
+                        if (startFlg) goto end;
+                        startIdx++;
+                        endIdx++;
+                        break;
+                    //ignore \r \n
+                    case '\r':
+                    case '\n':
+                        if (startFlg) goto end;
+                        else
+                        {
+                            endIdx++;
+                            if (statement[endIdx] == '\n')
+                                endIdx++;
+                            return "\r\n";
+                        }
+                    // statement	
+                    case '\'':
+                        endIdx = statement.IndexOf('\'', endIdx + 1);
+                        if (endIdx < startIdx) throw new ArgumentException("おかしいよ");
+                        goto end;
+                    // marks
+                    case ',':
+                    case '(':
+                    case ')':
+                    case ';':
+                        if (!startFlg) endIdx++;
+                        goto end;
+                    default:
+                        startFlg = true;
+                        endIdx++;
+                        break;
+                }
+            }
+            return statement.Substring(startIdx);
+            end:
+            return statement.Substring(startIdx, endIdx - startIdx);
+        }
+
+        abstract class BaseToken
+        {
+            public char Delimiter => ';';
+            public abstract void StatementEnd(ref string statement, ref int endIdx);
+        }
+
+        class NewLineToken : BaseToken
+        {
+            public override void StatementEnd(ref string statement, ref int endIdx)
+            {
+                while (endIdx < statement.Length)
+                {
+                    var ch = statement[endIdx];
+                    switch (ch)
+                    {
+                        case '\r':
+                        case '\n':
+                            return;
+                        default:
+                            endIdx++;
+                            break;
+                    }
+                }
+            }
+        }
+
+        class NormalToken : BaseToken
+        {
+            public override void StatementEnd(ref string statement, ref int endIdx)
+            {
+                var next = statement.IndexOf(Delimiter, endIdx);
+                endIdx = 0 < next ? next + 1 : statement.Length;
+            }
+        }
+        class ExecuteBlockToken : BaseToken
+        {
+            public override void StatementEnd(ref string statement, ref int endIdx)
+            {
+                var inBlockCount = 0;
+                var delimiter = Delimiter.ToString();
+
+                endIdx = statement.IndexOf("begin", endIdx, StringComparison.OrdinalIgnoreCase);
+
+                while (endIdx < statement.Length)
+                {
+                    var startIdx = endIdx;
+                    var word = GetWord(ref statement, ref startIdx, out endIdx);
+                    switch (word)
+                    {
+                        case var x when string.Compare(x, "begin", true) == 0:
+                            inBlockCount++;
+                            break;
+                        case var x when string.Compare(x, "end", true) == 0:
+                            inBlockCount--;
+                            break;
+                        case var x when x == delimiter:
+                            if (inBlockCount == 0)
+                            {
+                                return;
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        class BlockToken : BaseToken
+        {
+            public override void StatementEnd(ref string statement, ref int endIdx)
+            {
+                var inBlockCount = 0;
+                var delimiter = Delimiter.ToString();
+                while (endIdx < statement.Length)
+                {
+                    var startIdx = endIdx;
+                    var word = GetWord(ref statement, ref startIdx, out endIdx);
+                    switch (word)
+                    {
+                        case var x when string.Compare(x, "begin", true) == 0:
+                            inBlockCount++;
+                            break;
+                        case var x when string.Compare(x, "end", true) == 0:
+                            inBlockCount--;
+                            break;
+                        case var x when x == delimiter:
+                            if (inBlockCount == 0)
+                            {
+                                return;
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FAManagementStudioTests/Models/QueryAnalyzerTests.cs
@@ -0,0 +1,155 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+
+namespace FAManagementStudio.Models.Tests
+{
+    [TestClass()]
+    public class QueryAnalyzerTests
+    {
+        [TestMethod()]
+        public void GetStatementTest()
+        {
+            var input1 = "select * from A";
+            QueryAnalyzer.Analyze(input1).IsStructuralEqual(new[] { "select * from A" });
+
+            var input2 = "select * from A;";
+            QueryAnalyzer.Analyze(input2).IsStructuralEqual(new[] { "select * from A;" });
+
+            var input3 = "select * from A; select * from B";
+            QueryAnalyzer.Analyze(input3).IsStructuralEqual(new[] { "select * from A;", "select * from B" });
+
+            var input4 = @"" + Environment.NewLine +
+                      @" create trigger set_foo_primary for foo" + Environment.NewLine +
+                      @"before insert" + Environment.NewLine +
+                      @"" + Environment.NewLine +
+                      @"as begin" + Environment.NewLine +
+                      @"" + Environment.NewLine +
+                      @"new.a = gen_id(gen_foo, 1);" + Environment.NewLine +
+                      @"end" + Environment.NewLine +
+                      @";" + Environment.NewLine +
+                      @"  select* from A;" + Environment.NewLine +
+                      @" create table V(a integer, b nvarchar(5))";
+            QueryAnalyzer.Analyze(input4).IsStructuralEqual(new[] {
+                  @"create trigger set_foo_primary for foo" + Environment.NewLine +
+                      @"before insert" + Environment.NewLine +
+                      @"" + Environment.NewLine +
+                      @"as begin" + Environment.NewLine +
+                      @"" + Environment.NewLine +
+                      @"new.a = gen_id(gen_foo, 1);" + Environment.NewLine +
+                      @"end" + Environment.NewLine +
+                      @";",
+                "select* from A;", "create table V(a integer, b nvarchar(5))" });
+        }
+
+        [TestMethod()]
+        public void GetStatementTest2()
+        {
+            var input1 =
+@"--comment1
+create trigger set_foo_primary for foo
+before insert
+as begin
+new.a = gen_id(gen_foo, 1);
+end;
+
+--comment2  
+select *
+from test
+where a = 1;
+
+--comment3
+create trigger set_foo_primary for foo2
+before insert
+as begin
+new.a = gen_id(gen_foo, 1);
+end;
+
+select * from fuga where hoho = 'eeee'
+--comment4";
+
+            QueryAnalyzer.Analyze(input1).IsStructuralEqual(new[] {
+@"--comment1",
+@"create trigger set_foo_primary for foo
+before insert
+as begin
+new.a = gen_id(gen_foo, 1);
+end;",
+@"--comment2  ",
+@"select *
+from test
+where a = 1;",
+@"--comment3",
+@"create trigger set_foo_primary for foo2
+before insert
+as begin
+new.a = gen_id(gen_foo, 1);
+end;",
+@"select * from fuga where hoho = 'eeee'
+--comment4"
+});
+        }
+
+        [TestMethod()]
+        public void GetStatementTest3()
+        {
+            var input1 =
+@"--comment1
+select * from fuga where hoho = 'eeee' --comment2
+--comment3";
+
+            QueryAnalyzer.Analyze(input1).IsStructuralEqual(new[] {
+@"--comment1",
+@"select * from fuga where hoho = 'eeee' --comment2
+--comment3",});
+        }
+        [TestMethod()]
+        public void GetStatementTest4()
+        {
+            var input =
+@"-- ExecuteSample1
+execute block
+as
+declare i int = 0;
+begin
+  while (i < 128) do
+  begin
+    insert into AsciiTable values (:i, ascii_char(:i));
+    i = i + 1;
+  end end;
+
+--  ExecuteSample2
+execute block (x double precision = ?, y double precision = ?)
+returns (gmean double precision)
+as
+begin
+  gmean = sqrt(x*y);
+  suspend;
+end;";
+            QueryAnalyzer.Analyze(input).IsStructuralEqual(new[] {
+@"-- ExecuteSample1",
+@"execute block
+as
+declare i int = 0;
+begin
+  while (i < 128) do
+  begin
+    insert into AsciiTable values (:i, ascii_char(:i));
+    i = i + 1;
+  end end;",
+@"--  ExecuteSample2",
+@"execute block (x double precision = ?, y double precision = ?)
+returns (gmean double precision)
+as
+begin
+  gmean = sqrt(x*y);
+  suspend;
+end;"
+});
+        }
+    }
+}
