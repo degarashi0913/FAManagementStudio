ca754e8a1a39239840d18c34a3266c6a07412dd9
kowill <mob.koya@gmail.com>
2025-05-01 23:29:02 +0900

    refactoring: NewDatabaseSettingsViewModel

--- FAManagementStudio/ViewModels/NewDatabaseSettingsViewModel.cs
+++ FAManagementStudio/ViewModels/NewDatabaseSettingsViewModel.cs
@@ -1,56 +1,49 @@
 ﻿using FAManagementStudio.Common;
 using FirebirdSql.Data.FirebirdClient;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
 
-namespace FAManagementStudio.ViewModels
+namespace FAManagementStudio.ViewModels;
+
+public class NewDatabaseSettingsViewModel : ViewModelBase
 {
-    public class NewDatabaseSettingsViewModel : ViewModelBase
+
+    public NewDatabaseSettingsViewModel()
     {
-
-        public NewDatabaseSettingsViewModel()
+        OkCommand = new RelayCommand(() =>
         {
-            SetCommand();
-        }
+            MessengerInstance.Send(new MessageBase(this, "WindowClose"));
+        });
 
-        public string Path { get; set; }
-        public FirebirdType Type { get; set; }
-        public FbCharset CharSet { get; set; } = FbCharset.Utf8;
-
-        public ICommand OkCommand { get; private set; }
-        public ICommand CancelCommand { get; private set; }
-        public ICommand OpenFileDialog { get; private set; }
-        private void SetCommand()
+        CancelCommand = new RelayCommand(() =>
         {
-            OkCommand = new RelayCommand(() =>
+            Path = "";
+            MessengerInstance.Send(new MessageBase(this, "WindowClose"));
+        });
+
+        OpenFileDialog = new RelayCommand(() =>
+        {
+            using var dialog = new SaveFileDialog()
             {
-                MessengerInstance.Send(new MessageBase(this, "WindowClose"));
-            });
-            CancelCommand = new RelayCommand(() =>
-            {
-                Path = "";
-                MessengerInstance.Send(new MessageBase(this, "WindowClose"));
-            });
-            OpenFileDialog = new RelayCommand(() =>
-            {
-                using (var dialog = new SaveFileDialog())
-                {
-                    dialog.FileName = "NEWDB.FDB";
-                    dialog.DefaultExt = "fdb";
-                    dialog.Filter = "すべてのファイル(*.*)|*.*";
-                    dialog.OverwritePrompt = false;
-                    if (dialog.ShowDialog() != DialogResult.OK) return;
-                    if (File.Exists(dialog.FileName)) return;
-                    Path = dialog.FileName;
-                    RaisePropertyChanged(nameof(Path));
-                }
-            });
-        }
+                FileName = "NEWDB.FDB",
+                DefaultExt = "fdb",
+                Filter = "すべてのファイル(*.*)|*.*",
+                OverwritePrompt = false
+            };
+
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+            if (File.Exists(dialog.FileName)) return;
+            Path = dialog.FileName;
+            RaisePropertyChanged(nameof(Path));
+        });
     }
+
+    public string Path { get; set; } = string.Empty;
+    public FirebirdType Type { get; set; } = FirebirdType.Fb3;
+    public FbCharset CharSet { get; set; } = FbCharset.Utf8;
+
+    public ICommand OkCommand { get; private set; }
+    public ICommand CancelCommand { get; private set; }
+    public ICommand OpenFileDialog { get; private set; }
 }
