3f0578803ec9dfb3d78b1247393c236f0b9d0b31
kowill <mob.koya@gmail.com>
2025-05-03 11:20:58 +0900

    refactoring: GridHeaderDisplayNameBehavior

--- FAManagementStudio/Views/Behaviors/GridHeaderDisplayNameBehavior.cs
+++ FAManagementStudio/Views/Behaviors/GridHeaderDisplayNameBehavior.cs
@@ -4,41 +4,41 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using Microsoft.Xaml.Behaviors;
 
-namespace FAManagementStudio.Views.Behaviors
+namespace FAManagementStudio.Views.Behaviors;
+
+public class GridHeaderDisplayNameBehavior : Behavior<DataGrid>
 {
-    public class GridHeaderDisplayNameBehavior : Behavior<DataGrid>
+    public GridHeaderDisplayNameBehavior() { }
+    protected override void OnAttached()
     {
-        public GridHeaderDisplayNameBehavior() { }
-        protected override void OnAttached()
+        base.OnAttached();
+        AssociatedObject.AutoGeneratingColumn += AssociatedObject_AutoGeneratingColumn;
+    }
+
+    private const string NullString = "(DB_Null)";
+
+    private void AssociatedObject_AutoGeneratingColumn(object? sender, DataGridAutoGeneratingColumnEventArgs e)
+    {
+        if (e.Column.Header.ToString() is not { } propertyName) return;
+
+        e.Column.Header = ((DataView)AssociatedObject.ItemsSource).Table?.Columns[int.Parse(propertyName)].Caption;
+        ((DataGridBoundColumn)e.Column).Binding.TargetNullValue = NullString;
+
+        //ex.)boolean
+        if (e.Column is DataGridTextColumn column)
         {
-            base.OnAttached();
-            AssociatedObject.AutoGeneratingColumn += AssociatedObject_AutoGeneratingColumn;
+            //Null Value
+            var style = new Style(typeof(TextBlock));
+            var trigger = new DataTrigger { Binding = new Binding(propertyName), Value = null };
+            trigger.Setters.Add(new Setter(TextBlock.ForegroundProperty, new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.LightGray)));
+            style.Triggers.Add(trigger);
+            column.ElementStyle = style;
         }
+    }
 
-        private const string NullString = "(DB_Null)";
-
-        private void AssociatedObject_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
-        {
-            var propertyName = e.Column.Header.ToString();
-            e.Column.Header = (AssociatedObject.ItemsSource as DataView).Table.Columns[int.Parse(propertyName)].Caption;
-            ((DataGridBoundColumn)e.Column).Binding.TargetNullValue = NullString;
-
-            //ex.)boolean
-            if (e.Column is DataGridTextColumn column)
-            {
-                //Null Value
-                var style = new Style(typeof(TextBlock));
-                var trigger = new DataTrigger { Binding = new Binding(propertyName), Value = null };
-                trigger.Setters.Add(new Setter(TextBlock.ForegroundProperty, new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.LightGray)));
-                style.Triggers.Add(trigger);
-                column.ElementStyle = style;
-            }
-        }
-
-        protected override void OnDetaching()
-        {
-            AssociatedObject.AutoGeneratingColumn -= AssociatedObject_AutoGeneratingColumn;
-            base.OnDetaching();
-        }
+    protected override void OnDetaching()
+    {
+        AssociatedObject.AutoGeneratingColumn -= AssociatedObject_AutoGeneratingColumn;
+        base.OnDetaching();
     }
 }
