d0e4867381a7c5b6144764fba68fb95363fdb0d5
kowill <mob.koya@gmail.com>
2025-05-07 22:31:41 +0900

    refactoring: PathHistory

--- FAManagementStudio/Models/PathHistory.cs
+++ FAManagementStudio/Models/PathHistory.cs
@@ -1,77 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.Serialization;
-using System.Text;
 
-namespace FAManagementStudio.Models
+namespace FAManagementStudio.Models;
+
+[DataContract]
+class PathHistory
 {
-    [DataContract]
-    class PathHistory
+    [DataMember]
+    public required string[] Path { get; set; }
+}
+
+public class PathHistoryRepository
+{
+    public ObservableCollection<string> History { get; set; } = [];
+    private const string _fileName = "his";
+    public PathHistoryRepository()
     {
-        [DataMember]
-        public string[] Path { get; set; }
     }
 
-    public class PathHistoryRepository
+    public void LoadData(string entryPath)
     {
-        public ObservableCollection<string> History { get; set; } = new ObservableCollection<string>();
-        private const string _fileName = "his";
-        public PathHistoryRepository()
+        var path = Path.Combine(entryPath, _fileName);
+        if (!File.Exists(path)) return;
+        try
         {
-        }
+            using var stream = new FileStream(path, FileMode.Open);
+            var ser = new DataContractSerializer(typeof(PathHistory));
 
-        public void LoadData(string entryPath)
-        {
-            var path = Path.Combine(entryPath, _fileName);
-            if (!File.Exists(path)) return;
-            try
+            if (ser.ReadObject(stream) is PathHistory loadedHistory)
             {
-                using (var stream = new FileStream(path, FileMode.Open))
+                for (int i = 0; i < loadedHistory.Path.Length; i++)
                 {
-                    var ser = new DataContractSerializer(typeof(PathHistory));
-                    var loadedHistory = (PathHistory)ser.ReadObject(stream);
-                    for (int i = 0; i < loadedHistory.Path.Length; i++)
-                    {
-                        History.Add(loadedHistory.Path[i]);
-                    }
+                    History.Add(loadedHistory.Path[i]);
                 }
             }
-            //失敗したらそのまま
-            catch
-            {
-            }
         }
-
-        public void DataAdd(string newPath)
+        //失敗したらそのまま
+        catch
         {
-            if (History.Contains(newPath))
-            {
-                History.Remove(newPath);
-            }
-
-            if (10 <= History.Count)
-            {
-                History.RemoveAt(History.Count - 1);
-            }
-            History.Insert(0, newPath);
         }
+    }
 
-        public void SaveData(string entryPath)
+    public void DataAdd(string newPath)
+    {
+        int index = History.IndexOf(newPath);
+
+        if (0 <= index)
         {
-            var path = Path.Combine(entryPath, _fileName);
-            var data = new PathHistory();
-            data.Path = History.ToArray();
-
-            using (var stream = new FileStream(path, FileMode.OpenOrCreate))
-            {
-                var ser = new DataContractSerializer(typeof(PathHistory));
-                ser.WriteObject(stream, data);
-            }
+            if (0 == index) return;
+            History.RemoveAt(index);
         }
+
+        if (10 <= History.Count)
+        {
+            History.RemoveAt(History.Count - 1);
+        }
+        History.Insert(0, newPath);
+    }
+
+    public void SaveData(string entryPath)
+    {
+        var path = Path.Combine(entryPath, _fileName);
+        var data = new PathHistory
+        {
+            Path = [.. History]
+        };
+
+        using var stream = new FileStream(path, FileMode.OpenOrCreate);
+        var ser = new DataContractSerializer(typeof(PathHistory));
+        ser.WriteObject(stream, data);
     }
 }
