fede10d0ac8aa1cebb0d08d37224c401b77c6ab7
kowill <mob.koya@gmail.com>
2025-05-13 21:30:13 +0900

    refactoring: CompletionData

--- FAManagementStudio.Controls/Common/CompletionData.cs
+++ FAManagementStudio.Controls/Common/CompletionData.cs
@@ -2,73 +2,50 @@
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Editing;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
 
-namespace FAManagementStudio.Controls.Common
+namespace FAManagementStudio.Controls.Common;
+
+public class CompletionData(string text) : ICompletionData
 {
-    public class CompletionData : ICompletionData
+    public ImageSource? Image => null;
+
+    public string Text { get; private set; } = text;
+
+    public object Content => Text;
+
+
+    public object Description => "Description for " + Text;
+
+    public double Priority => 0.0;
+
+    public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
     {
-        public CompletionData(string text)
+        textArea.Document.Replace(CurrentWordSegment(completionSegment, textArea.Document.Text), this.Text);
+    }
+
+    private static TextSegment CurrentWordSegment(ISegment seg, string text)
+    {
+        var marks = new[] { '\r', '\n', ' ', '.' };
+        var str = seg.Offset - 1;
+
+        if (text.Length < 1 || marks.Contains(text[str])) return new TextSegment { StartOffset = seg.Offset, EndOffset = seg.EndOffset };
+
+        while (0 < str)
         {
-            this.Text = text;
+            var c = text[str - 1];
+            if (marks.Contains(c)) break;
+            str--;
         }
 
-        public ImageSource Image
+        var end = seg.EndOffset;
+        while (end < text.Length)
         {
-            get { return null; }
+            var c = text[end];
+            if (marks.Contains(c)) break;
+            end++;
         }
-
-        public string Text { get; private set; }
-
-        public object Content
-        {
-            get { return this.Text; }
-        }
-
-        public object Description
-        {
-            get { return "Description for " + this.Text; }
-        }
-
-        public double Priority
-        {
-            get
-            {
-                return 0.0;
-            }
-        }
-
-        public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
-        {
-            textArea.Document.Replace(CurrentWordSegment(completionSegment, textArea.Document.Text), this.Text);
-        }
-
-        private TextSegment CurrentWordSegment(ISegment seg, string text)
-        {
-            var marks = new[] { '\r', '\n', ' ', '.' };
-            var str = seg.Offset - 1;
-
-            if (text.Length < 1 || marks.Contains(text[str])) return new TextSegment { StartOffset = seg.Offset, EndOffset = seg.EndOffset };
-
-            while (0 < str)
-            {
-                var c = text[str - 1];
-                if (marks.Contains(c)) break;
-                str--;
-            }
-
-            var end = seg.EndOffset;
-            while (end < text.Length)
-            {
-                var c = text[end];
-                if (marks.Contains(c)) break;
-                end++;
-            }
-            return new TextSegment { StartOffset = str, EndOffset = end };
-        }
+        return new TextSegment { StartOffset = str, EndOffset = end };
     }
 }
